d673b7e218332b843414f1081e85faa4b432fbd7
Ayush Mathur <ayushmathur919@gmail.com>
2024-02-20 21:01:26 +0530

    updated card

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,31 +1,31 @@
-import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, CardActions, Button, Box } from "@mui/material";
 
-export default function Page() {
-    return (
-        <> I'm page
-            <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="https://m.media-amazon.com/images/I/715igBNcyEL._AC_UF1000,1000_QL80_.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-             
-        </>
-    )
+export default function Page({imageUrl}) {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", gap: "10", margin:"10px 0 10px 0" }}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          image={imageUrl}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions sx={{display:"flex",justifyContent:"space-between"}}>
+          <Button size="small" variant="outlined" color="warning">Add To Cart</Button>
+          <Button size="small" variant="contained" color="primary">Buy Now</Button>
+        </CardActions>
+      </Card>
+
+    </Box>
+  )
 }
 
