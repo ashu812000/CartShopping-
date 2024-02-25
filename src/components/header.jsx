119bc1aee8461968fc7a240876e5d8ef5d36d0c6
Ayush Mathur <ayushmathur919@gmail.com>
2024-02-25 23:08:09 +0530

    added logout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,8 @@
-import {Box} from '@mui/material';
+import {Box, Button} from '@mui/material';
 import React from 'react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import {useNavigate} from "react-router-dom";
+import LogoutIcon from '@mui/icons-material/Logout';
 // import "./style.css"
 
 const Header = () => {
@@ -12,6 +13,14 @@ const Header = () => {
             <Box className="cart-icon">
                 <Box className="order-history" onClick={() => navigate("/orderHistory")}>Order History</Box>
                 <ShoppingCartIcon sx={{cursor: "pointer"}} size={24} onClick={() => navigate("/cart")}/>
+                <Button variant={"contained"}
+                        color={"error"}
+                        onClick={() => {
+                            localStorage.removeItem('email')
+                            navigate("/")
+                        }
+                        }>Logout <LogoutIcon
+                    sx={{paddingLeft: "5px"}}/></Button>
             </Box>
         </Box>
     );
